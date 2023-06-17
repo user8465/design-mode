2dc80a486d19ecc8333eb47a9f28eea0d39c9b79
tanyu <1187315152@qq.com>
2023-06-17 16:46:33 +0800

    2023.6.17

---
+++ src/main/java/com/DEMO_单例设计模式/demo1/Singleton.java
@@ -0,0 +1,13 @@
+package com.DEMO_单例设计模式.demo1;
+
+/**
+ * @Author tanyu
+ * @Date 2023/6/17 16:45
+ * @PackageName: com.DEMO_单例设计模式.demo1
+ * @ClassName: Singleton
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class Singleton {
+
+}
